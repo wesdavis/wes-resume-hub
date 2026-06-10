48e2f930d786efc60179cef904cf710472332c5b
wesdavis <westleyhdavis@gmail.com>
2026-06-09 18:02:24 -0600

    cmt out chatwidget from app.jsx temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Topbar from './components/layout/Topbar'
 import KanbanBoard from './components/kanban/KanbanBoard'
-import ChatWidget from './components/chatbot/ChatWidget'
+//import ChatWidget from './components/chatbot/ChatWidget'
 import { Analytics } from "@vercel/analytics/react";
 
 function App() {
